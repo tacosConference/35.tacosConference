6fd92e7be5879c7a82ef279f94dfba721b0eb9fe
TuringTux <hi@turingtux.me>
2025-10-27 09:47:25 +0100

    Improve newsletter form

--- src/lib/components/NewsletterForm.svelte
+++ src/lib/components/NewsletterForm.svelte
@@ -8,21 +8,11 @@
 	let checkClassName = `tacos-check-${color}`;
 </script>
 
-<form action="https://newsletter.fachschaft.cl.uni-heidelberg.de/forms/nfrm_BzLMaLXv" method="post">
-    <legend>TaCoS Newsletter</legend>
+<form action="{m.newsletter_form_url()}" method="post">
+    <legend>{m.newsletter_legend()}</legend>
+	<p>{m.newsletter_description()}</p>
+
 	<div class="row g-2">
-		<div class="col-md">
-			<div class="form-floating mb-3">
-				<input
-					name="contact[first_name]"
-					type="text"
-					class="form-control {inputClassName}"
-					id="firstName"
-					placeholder="name@example.com"
-				/>
-				<label for="firstName">First name</label>
-			</div>
-		</div>
 		<div class="col-md">
 			<div class="form-floating">
 				<input
@@ -30,14 +20,25 @@
 					type="email"
 					class="form-control {inputClassName}"
 					id="email"
-					placeholder="Password"
+					required
 				/>
-				<label for="email">E-Mail</label>
+				<label for="email">{m.email()}</label>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="form-floating mb-3">
+				<input
+					name="contact[first_name]"
+					type="text"
+					class="form-control {inputClassName}"
+					id="firstName"
+				/>
+				<label for="firstName">{m.first_name()}</label>
 			</div>
 		</div>
 	</div>
 	<div class="row g-2">
 		<input aria-hidden="true" autocomplete="off" name="h[url]" style="display: none" />
-		<button class="btn btn-primary {colorClassName}"> Subscribe </button>
+		<button class="btn btn-primary {colorClassName}">{m.subscribe()}</button>
 	</div>
 </form>
