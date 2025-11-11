3422bb90ec068951906f7fc9133b123b84ae2125
TuringTux <hi@turingtux.me>
2025-11-11 17:07:49 +0100

    Make favicon and background based

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { hasContext, onMount, setContext } from 'svelte';
 	import '$lib/fonts.css';
+	import { base } from '$app/paths';
 	import '../app.scss';
 	import HeaderBar from '$lib/components/HeaderBar.svelte';
 	import Transition from '$lib/components/Transition.svelte';
@@ -16,10 +17,10 @@
 </script>
 
 <svelte:head>
-	<link rel="apple-touch-icon" sizes="180x180" href="/media/favicon/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/media/favicon/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/media/favicon/favicon-16x16.png" />
-	<link rel="manifest" href="/media/favicon/site.webmanifest" />
+	<link rel="apple-touch-icon" sizes="180x180" href="{base}/media/favicon/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="{base}/media/favicon/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="{base}/media/favicon/favicon-16x16.png" />
+	<link rel="manifest" href="{base}/media/favicon/site.webmanifest" />
 </svelte:head>
 
 <HeaderBar color={color.color} />
@@ -33,9 +34,9 @@
   z-index: -1;
   opacity: 1;
  "
-	fullSrc="/media/theoretikum/Theoretikum-upscale-3888.webp"
-	previewSrc="/media/theoretikum/Theoretikum-mini.webp"
-	srcset="/media/theoretikum/Theoretikum-upscale-3888.webp 2x, /media/theoretikum/Theoretikum-upscale-3888.webp 3888w, /media/theoretikum/Theoretikum-upscale-1920.webp 1920w, /media/theoretikum/Theoretikum-upscale-1280.webp 1280w"
+	fullSrc="{base}/media/theoretikum/Theoretikum-upscale-3888.webp"
+	previewSrc="{base}/media/theoretikum/Theoretikum-mini.webp"
+	srcset="{base}/media/theoretikum/Theoretikum-upscale-3888.webp 2x, {base}/media/theoretikum/Theoretikum-upscale-3888.webp 3888w, {base}/media/theoretikum/Theoretikum-upscale-1920.webp 1920w, {base}/media/theoretikum/Theoretikum-upscale-1280.webp 1280w"
 	alt="Hauptgebäude der Universität Heidelberg (Theoretikum) bei Sonnenuntergang"
 	loading="lazy"
 	sizes="(max-width: 1279px) 1280px, (max-width: 1919px) 1920px, 3888px"
