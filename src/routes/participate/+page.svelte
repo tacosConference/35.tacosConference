d103493f7455a243170b66cfb095e8d52656dfbd
TuringTux <hi@turingtux.me>
2026-02-04 18:01:26 +0100

    Add privacy explanations

--- src/routes/participate/+page.svelte
+++ src/routes/participate/+page.svelte
@@ -31,6 +31,22 @@
 						{m.participate_description()} <Link href="/" text="{m.subscribe_to_the_newsletter()}" />.
 					</p>
 
+					<h3>{m.participate_privacy()}</h3>
+					<ul class="mb-4">
+						<li><strong>{m.first_name()}</strong> {m.and()} <strong>{m.last_name()}</strong>: {m.privacy_name()}</li>
+						<li><strong>{m.pronouns()}</strong>: {m.privacy_pronouns()}</li>
+						<li><strong>{m.email()}</strong>: {m.privacy_email()}</li>
+						<li><strong>{m.affiliation()}</strong>: {m.privacy_affiliation()}</li>
+						<li><strong>{m.study_programme()}</strong>: {m.privacy_study_programme()}</li>
+						<li><strong>{m.languages()}</strong>: {m.privacy_languages()}</li>
+						<li><strong>{m.quote_left()}{m.previously_at_tacos()}{m.quote_right()}</strong>: {m.privacy_previously_at_tacos()}</li>
+						<li><strong>{m.remarks()}</strong>: {m.privacy_remarks()}</li>
+					</ul>
+					
+					<p class="mb-4">
+						{m.participate_privacy_description()}
+					</p>
+
 					<h3>{m.participate_price()}</h3>
 					<p class="mb-4">
 						{m.participate_price_description()}
