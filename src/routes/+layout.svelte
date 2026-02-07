a9e1d777cdde755b56aa6553c202c3d076fbffb5
chefexperte <aaron+git@chefexperte.de>
2026-02-07 21:04:47 +0100

    Improve image loading in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,14 @@
 	import HeaderBar from '$lib/components/HeaderBar.svelte';
 	import Transition from '$lib/components/Transition.svelte';
 	import ProgressiveImage from '$lib/components/ProgressiveImage.svelte';
+	import appleTouchIcon from '$lib/assets/favicon/apple-touch-icon.png';
+	import favicon32 from '$lib/assets/favicon/favicon-32x32.png';
+	import favicon16 from '$lib/assets/favicon/favicon-16x16.png';
+	import manifest from '$lib/assets/favicon/site.webmanifest';
+	import theoretikumFull from '$lib/assets/theoretikum/Theoretikum-upscale-3888.webp';
+	import theoretikumMini from '$lib/assets/theoretikum/Theoretikum-mini.webp';
+	import theoretikum1920 from '$lib/assets/theoretikum/Theoretikum-upscale-1920.webp';
+	import theoretikum1280 from '$lib/assets/theoretikum/Theoretikum-upscale-1280.webp';
 	let { children, data } = $props();
 	let color = $state({ color: data.color });
 	if (!hasContext('color')) setContext('color', color);
@@ -19,10 +27,10 @@
 </script>
 
 <svelte:head>
-	<link rel="apple-touch-icon" sizes="180x180" href="{base}/media/favicon/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="{base}/media/favicon/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="{base}/media/favicon/favicon-16x16.png" />
-	<link rel="manifest" href="{base}/media/favicon/site.webmanifest" />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+	<link rel="manifest" href={manifest} />
 </svelte:head>
 
 <HeaderBar color={color.color} />
@@ -36,9 +44,9 @@
   z-index: -1;
   opacity: 1;
  "
-	fullSrc="{base}/media/theoretikum/Theoretikum-upscale-3888.webp"
-	previewSrc="{base}/media/theoretikum/Theoretikum-mini.webp"
-	srcset="{base}/media/theoretikum/Theoretikum-upscale-3888.webp 2x, {base}/media/theoretikum/Theoretikum-upscale-3888.webp 3888w, {base}/media/theoretikum/Theoretikum-upscale-1920.webp 1920w, {base}/media/theoretikum/Theoretikum-upscale-1280.webp 1280w"
+	fullSrc={theoretikumFull}
+	previewSrc={theoretikumMini}
+	srcset="{theoretikumFull} 2x, {theoretikumFull} 3888w, {theoretikum1920} 1920w, {theoretikum1280} 1280w"
 	alt="Hauptgebäude der Universität Heidelberg (Theoretikum) bei Sonnenuntergang"
 	loading="lazy"
 	sizes="(max-width: 1279px) 1280px, (max-width: 1919px) 1920px, 3888px"
